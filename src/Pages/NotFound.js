9bc88707e6f7cc380d7aee0f0a7dcaa9b26403e0
Karolina <hebiame@gmail.com>
2019-02-20 23:34:00 +0100

    Add alt attributes

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -7,14 +7,14 @@ import '../Scss/NotFoundPage.scss';
 const NotFound = () => (
   <main className='notfound-page bg-white justify-content-center d-flex align-items-center'>
     <div className='col-12 col-md-6 text-center text-md-left'>
-      <img src={space} className='d-md-none' />
+      <img src={space} className='d-md-none' alt='not found'/>
       <h1 className='mt-3 mt-md-0'>Hmm.</h1>
       <p className=''>It seems that you're lost in a perpetual black hole. Let us help guide you out and get you back home.</p>
       <Link to='/'>
         <Button color='makise-primary ml-0 px-2 py-1 py-md-2 px-md-3 text-uppercase'>Help me out</Button>
       </Link>
     </div>
-    <div className='d-none d-md-inline-block col-md-6 text-center'><img src={space} /></div>
+    <div className='d-none d-md-inline-block col-md-6 text-center'><img src={space} alt='not found'/></div>
   </main>
 );
 
