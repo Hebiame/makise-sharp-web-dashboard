529d80bf88d61ff2a412c8744e9b6bfec4629842
Hebiame <19415637+Hebiame@users.noreply.github.com>
2019-02-26 21:57:09 +0100

    Add random description text to homepage (#2)

---
+++ src/Pages/Start/RandomDescription.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class RandomDescription extends Component {
+
+  descriptions = [
+    "Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Cras eu nulla ligula. Ut at elementum nisl.",
+    "Nulla in turpis non nunc. Suspendisse potenti. Cras nec diam eros, dapibus eget, ante.",
+    "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ut orci id felis.",
+    "In euismod quis, egestas consequat et, dictum aliquet id, elementum eros cursus quis, accumsan vestibulum."
+  ];
+
+  render() {
+    return <p>{this.descriptions[Math.floor(Math.random() * this.descriptions.length)]}</p>;
+  }
+}
+
+export default RandomDescription;
